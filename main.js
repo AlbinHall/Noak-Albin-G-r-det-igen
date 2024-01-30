b474007198e3673a742b5980845ff805c01ba1a7
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2024-01-30 15:42:07 +0100

    API?

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 let colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'white'];
+
+var API_KEY = '42114230-518f9984a7b51cfb128a38f28';
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});
